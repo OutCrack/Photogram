138d6b5d384d69c66f52bcac85e61eef0c5c35cb
kasior251 <s305327@stud.hioa.no>
2018-02-12 09:52:44 +0100

    Update main.ts

--- app/main.ts
+++ app/main.ts
@@ -2,10 +2,7 @@ import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 
 import { AppModule } from "./app.module";
 
-import * as tnsOAuthModule from 'nativescript-oauth';
-
 const firebase = require("nativescript-plugin-firebase");
-
 
 platformNativeScriptDynamic().bootstrapModule(AppModule);
 
