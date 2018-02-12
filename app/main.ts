fd5be23f50342ec23f443def745e34901ac57e93
kasior251 <s305327@stud.hioa.no>
2018-02-12 09:51:56 +0100

    Update main.ts

--- app/main.ts
+++ app/main.ts
@@ -9,11 +9,3 @@ const firebase = require("nativescript-plugin-firebase");
 
 platformNativeScriptDynamic().bootstrapModule(AppModule);
 
-/*firebase.init({
-    onAuthStateChanged: function(data) { // optional but useful to immediately re-logon the user when he re-visits your app
-      console.log(data.loggedIn ? "Logged in to firebase" : "Logged out from firebase");
-      if (data.loggedIn) {
-        console.log("user's email address: " + (data.user.email ? data.user.email : "N/A"));
-      }
-    }
-  });*/
