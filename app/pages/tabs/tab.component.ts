dfff3c76469b51242c8803a3b46846a13b333b08
kasior251 <s305328@stud.hioa.no>
2018-05-14 21:01:41 +0200

    fixed icons

--- app/pages/tabs/tab.component.ts
+++ app/pages/tabs/tab.component.ts
@@ -6,11 +6,6 @@ import { Data } from "../../shared/Data";
 import { RouterExtensions } from "nativescript-angular";
 const firebase = require("nativescript-plugin-firebase");
 
-export class DataItem {
-    constructor(public itemDesc: string) { 
-    }
-}
-
 @Component({
     selector: "tab",
     moduleId: module.id,
@@ -19,25 +14,21 @@ export class DataItem {
 })
 
 export class TabComponent {
-    public items: Array<DataItem>;
-    public activeTab: string;
-    public selectedIndex: number = 0;
     isHome: boolean = true;
     isSearch: boolean = false;
     isGallery: boolean = false;
     isNotification: boolean = false;
     isProfile: boolean = false;
-    //public firstName: string;
-    //public lastName: string;
-    public tabName: string = "feed";
+    public tabName: string = "Feed";
+
 
     constructor(private routerExtensions: RouterExtensions, private router: Router, private page: Page, protected data: Data) {
         this.tabName = "feed";
     }
+
     onCamera() {
         console.log("Camera tapped.");
         this.router.navigate(["/image"]);
-        //Kjører kamera funksjon------
     }
 
     onBackButtonTap() {
@@ -49,24 +40,23 @@ export class TabComponent {
     }
 
     onNewEvent() {
-        console.log("New event tapped.");
-        this.tabName = "new event";
+
+        this.tabName = "New event";
         this.router.navigate(["/newEvent"]);
     }
 
     onInvitation() {
-        console.log("New invitation tapped");
         this.router.navigate(["/invitation"]);
+        this.tabName="Invitation";
     }
 
     onHome() {
-        console.log("Home-tab tapped.");
         this.isHome = true;
         this.isSearch = false;
         this.isGallery = false;
         this.isNotification = false;
         this.isProfile = false;
-        this.tabName = "feed";
+        this.tabName = "Feed";
     }
 
     onSearch() {
@@ -76,42 +66,34 @@ export class TabComponent {
         this.isGallery = false;
         this.isNotification = false;
         this.isProfile = false;
-        this.tabName = "search";
-        //console.log("Users name" + this.firstName);
-        //console.log("Last name" + this.lastName);
+        this.tabName = "Search";
     }
 
     onGallery() {
-        console.log("Gallery-tab tapped.");
         this.isHome = false;
         this.isSearch = false;
         this.isGallery = true;
         this.isNotification = false;
         this.isProfile = false; 
-        this.tabName = "gallery";          
+        this.tabName = "My gallery";          
     }
 
     onNotification() {
-        console.log("Notification-tab tapped.");
         this.isHome = false;
         this.isSearch = false;
         this.isGallery = false;
         this.isNotification = true;
         this.isProfile = false; 
-        this.tabName = "notifications";  
+        this.tabName = "Events";  
     }
 
     onProfile() {
-        console.log("Profile-tab tapped.");
         this.isHome = false;
         this.isSearch = false;
         this.isGallery = false;
         this.isNotification = false;
         this.isProfile = true;  
-        //this.firstName = this.data.storage["firstName"];
-        //this.lastName = this.data.storage["lastName"];
-        this.tabName = "profile";
-        console.log("Tabname " + this.tabName);
+        this.tabName = "Profile";
     }
 
     tabViewIndexChange(index: number) {
